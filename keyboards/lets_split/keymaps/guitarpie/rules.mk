fb4c33251a46960b35fa2fe24b27ef5a84c23b12
Chris Cavanaugh <chriswcav@gmail.com>
2021-01-13 15:52:54 -0500

    intial commit of kemap. layer 0 wasn't working need to investigate

---
+++ keyboards/lets_split/keymaps/guitarpie/rules.mk
@@ -0,0 +1,8 @@
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in 
+#   the appropriate keymap folder that will get included automatically
+#
+UNICODE_ENABLE = no         # Unicode
+UNICODEMAP_ENABLE = yes
+
+TAP_DANCE_ENABLE = yes
